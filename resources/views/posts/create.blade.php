83d2ce131485ecba65ea75463e87e64d6d127531
Yusuke Kamiyashiki <k.yusuke.0723@gmail.com>
2019-04-12 11:50:43 +0900

    fix:typo

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -11,10 +11,10 @@
 
       {!! Form::open(array('route' => 'posts.store')) !!}
         {{ Form::label('title', 'Title:') }}
-        {{ Form::text('title', null, array('class' => 'form-controll')) }}
+        {{ Form::text('title', null, array('class' => 'form-control')) }}
 
         {{ Form::label('body', 'Post Body:') }}
-        {{ Form::textarea('body', null, array('class' => 'form-controll')) }}
+        {{ Form::textarea('body', null, array('class' => 'form-control')) }}
 
         {{ Form::submit('Create Post', array('class' => 'btn btn-success btn-lg btn-block', 'style' => 'margin-top: 20px;')) }}
       {!! Form::close() !!}
