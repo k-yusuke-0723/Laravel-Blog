de04db8f69e3ae07cde604627a7ac1c1ff362769
Yusuke Kamiyashiki <k.yusuke.0723@gmail.com>
2019-04-12 15:52:13 +0900

    バリデーション時にエラービューを表示

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -2,6 +2,12 @@
 
 @section('title', '| Create New Post')
 
+@section('stylesheet')
+
+  {!! Html::style('css/parsley.css') !!}
+
+@endsection
+
 @section('content')
 
   <div class="row">
@@ -9,12 +15,12 @@
       <h1>Create New Post</h1>
       <hr>
 
-      {!! Form::open(array('route' => 'posts.store')) !!}
+      {!! Form::open(array('route' => 'posts.store', 'data-parsley-validate' => '')) !!}
         {{ Form::label('title', 'Title:') }}
-        {{ Form::text('title', null, array('class' => 'form-control')) }}
+        {{ Form::text('title', null, array('class' => 'form-control', 'required' => '')) }}
 
         {{ Form::label('body', 'Post Body:') }}
-        {{ Form::textarea('body', null, array('class' => 'form-control')) }}
+        {{ Form::textarea('body', null, array('class' => 'form-control', 'required' => '')) }}
 
         {{ Form::submit('Create Post', array('class' => 'btn btn-success btn-lg btn-block', 'style' => 'margin-top: 20px;')) }}
       {!! Form::close() !!}
@@ -23,3 +29,8 @@
 
 @endsection
 
+@section('scripts')
+
+  {!! Html::script('js/parsley.min.js') !!}
+
+@endsection
