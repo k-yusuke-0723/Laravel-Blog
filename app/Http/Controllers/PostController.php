bf99911b10692bef1412931b650d0439e8464fd7
Yusuke Kamiyashiki <k.yusuke.0723@gmail.com>
2019-04-12 14:28:54 +0900

    DBへの保存

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -4,6 +4,10 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+use App\Post;
+
 class PostController extends Controller
 {
     /**
@@ -34,7 +38,23 @@ class PostController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        //データをvalidateする
+        $this -> validate($request, array(
+            'title' => 'required|max:255',
+            'body'  => 'required'
+
+        ));
+
+        // store in the database(データベースに格納する)
+        $post = new Post;
+
+        $post -> title = $request -> title;
+        $post -> body  = $request -> body;
+
+        $post -> save();
+        // redirect to another page(ページ遷移させる)
+
+        return redirect() -> route('posts.show', $post ->id);
     }
 
     /**
