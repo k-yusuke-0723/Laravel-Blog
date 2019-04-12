1d03c9c32ed9242255d0ebc97b691e23ecb84916
Yusuke Kamiyashiki <k.yusuke.0723@gmail.com>
2019-04-12 18:33:12 +0900

    投稿後にshowページへ遷移しフラッシュを表示

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -7,6 +7,7 @@ use Illuminate\Http\Request;
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
 use App\Post;
+use Session;
 
 class PostController extends Controller
 {
@@ -54,6 +55,8 @@ class PostController extends Controller
         $post -> save();
         // redirect to another page(ページ遷移させる)
 
+        Session::flash('success', 'The blog post was successfully save!');
+
         return redirect() -> route('posts.show', $post ->id);
     }
 
@@ -65,7 +68,7 @@ class PostController extends Controller
      */
     public function show($id)
     {
-        //
+        return view('posts.show');
     }
 
     /**
