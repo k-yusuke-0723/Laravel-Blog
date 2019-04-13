df12cb92ee143dfb54b5c73a4a087ca0f7cb59aa
Yusuke Kamiyashiki <k.yusuke.0723@gmail.com>
2019-04-13 10:35:52 +0900

    時間表示欄の用意

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -4,8 +4,26 @@
 
 @section('content')
 
-  <h1>{{ $post->title }}</h1>
+  <div class="row">
+    <div class="col-md-8">
+      <h1>{{ $post->title }}</h1>
+      <p class="lead">{{ $post->body }}</p>
+    </div>
 
-  <p class="lead">{{ $post->body }}</p>
 
+    <div class="col-md-4">
+      <div class="well">
+
+        <dl class="dl-horizontal">
+          <dt>Create At:</dt>
+          <dd>time</dd>
+        </dl>
+        <dl class="dl-horizontal">
+          <dt>Last Updated:</dt>
+          <dd>time</dd>
+        </dl>
+
+      </div>
+    </div>
+  </div>
 @endsection
