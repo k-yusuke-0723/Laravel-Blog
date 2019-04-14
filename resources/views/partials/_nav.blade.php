90e06ff0165266061bcb3fbecca73cdbb45b3bd9
Yusuke Kamiyashiki <k.yusuke.0723@gmail.com>
2019-04-14 22:08:16 +0900

    ブログタイトルの修正

--- resources/views/partials/_nav.blade.php
+++ resources/views/partials/_nav.blade.php
@@ -1,6 +1,6 @@
 <!-- Default Bootstrap Navbar -->
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="/">Laravel Blog</a>
+  <a class="navbar-brand" href="/">KMYSK Blog</a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
     </button>
