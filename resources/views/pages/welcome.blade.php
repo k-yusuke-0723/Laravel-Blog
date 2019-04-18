a843042c5b2897eac547ac310757713fc46fb846
Yusuke Kamiyashiki <k.yusuke.0723@gmail.com>
2019-04-18 11:10:32 +0900

    welcomeページへ表示

--- resources/views/pages/welcome.blade.php
+++ resources/views/pages/welcome.blade.php
@@ -7,54 +7,35 @@
 @section('title', '| Homepage')
 
 @section('content')
-    <div class="container">
-        <div class="row">
-            <div class="col-md-12">
-                <div class="jumbotron">
-                  <h1 class="display-4">ようこそ、ブログへ！</h1>
-                  <p class="lead">来てくれてありがとう！</p>
-                  <hr class="my-4">
-                  <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
-                  <a class="btn btn-primary btn-lg" href="#" role="button">Popular post</a>
-                </div>
+  <div class="container">
+      <div class="row">
+          <div class="col-md-12">
+              <div class="jumbotron">
+                <h1 class="display-4">ようこそ、ブログへ！</h1>
+                <p class="lead">来てくれてありがとう！</p>
+                <hr class="my-4">
+                <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
+                <a class="btn btn-primary btn-lg" href="#" role="button">Popular post</a>
+              </div>
+          </div>
+      </div> <!-- headerのrowクラスの終わり -->
+
+      <div class = "row">
+        <div class = "col-md-8">
+
+          @foreach($posts as $post)
+
+            <div class="post">
+              <h3>{{ $post -> title }}</h3>
+              <p>{{ $post -> body }}</p>
+              <a href="#" class="btn btn-primary">Read More</a>
             </div>
-        </div> <!-- headerのrowクラスの終わり -->
 
-        <div class = "row">
-            <div class = "col-md-8">
-                <div class="post">
-                    <h3>Post Title</h3>
-                    <p>サッカー好きです</p>
-                    <a href="#" class="btn btn-primary">Read More</a>
-                </div>
+            <hr>
 
-                <hr>
+          @endforeach
 
-                <div class="post">
-                    <h3>Post Title</h3>
-                    <p>サッカー好きです</p>
-                    <a href="#" class="btn btn-primary">Read More</a>
-                </div>
-
-                <hr>
-
-                <div class="post">
-                    <h3>Post Title</h3>
-                    <p>サッカー好きです</p>
-                    <a href="#" class="btn btn-primary">Read More</a>
-                </div>
-
-                <hr>
-
-                <div class="post">
-                    <h3>Post Title</h3>
-                    <p>サッカー好きです</p>
-                    <a href="#" class="btn btn-primary">Read More</a>
-                </div>
-
-                <hr>
-
-            </div>
+        </div>
 
             <div class="col-md-3 col-md-offset-1">
                 <h2>Sideber</h2>
