1e71150652d9df817b5aa2486da9878bd7f125d8
Yusuke Kamiyashiki <k.yusuke.0723@gmail.com>
2019-04-21 09:54:40 +0900

    blogcontrollerの設定

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -7,6 +7,12 @@ use App\Post;
 
 class BlogController extends Controller
 {
+
+  public function getIndex() {
+    $posts = Post::paginate(10);
+
+    return view('blog.index') -> withPosts($posts);
+  }
   public function getSingle($slug) {
     // slugに基づいてDBからデータを取得
     $post = Post::where('slug', '=', $slug) -> first();
