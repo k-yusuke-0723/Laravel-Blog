68701c262d6bd4d6b36f5da492f28dffe08f24cf
Yusuke Kamiyashiki <k.yusuke.0723@gmail.com>
2019-04-23 07:42:28 +0900

    登録ページの修正

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -8,8 +8,20 @@
       <div class="col-md-6 col-md-offset-3">
           {!! Form::open() !!}
 
-            {{ Form::label('name', "Name:") }}
+            {{ Form::label('name', 'Name:') }}
             {{ Form::text('name', null, ['class' => 'form-control']) }}
+
+            {{ Form::label('email', 'Email:') }}
+            {{ Form::email('email', null, ['class' => 'form-control']) }}
+
+            {{ Form::label('password', 'Password:') }}
+            {{ Form::password('password', ['class' => 'form-control']) }}
+
+            {{ Form::label('password_confirmation', 'Confirm Password:') }}
+            {{ Form::password('password_confirmation', ['class' => 'form-control']) }}
+
+            {{ Form::submit('Register', ['class' => 'btn btn-primary btn-block']) }}
+
           {!! Form::close() !!}
       </div>
   </div>
