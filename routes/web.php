c18ba98cb5c94b8c00a9043f3f47b42214689626
Yusuke Kamiyashiki <k.yusuke.0723@gmail.com>
2019-04-22 12:50:32 +0900

    ログイン画面の作成

--- routes/web.php
+++ routes/web.php
@@ -13,13 +13,13 @@
 
 Route::group(['middleware' => ['web']], function() {
   // 認証ルート（ログイン・ログアウト）
-  Route::get('auth/login', 'Auth\AuthController@getLogin');
-  Route::post('auth/login', 'Auth\AuthController@postLogin');
-  Route::get('auth/logout', 'Auth\AuthController@getLogout');
+  //Route::get('auth/login', 'Auth\AuthController@getLogin');
+  //Route::post('auth/login', 'Auth\AuthController@postLogin');
+  //Route::get('auth/logout', 'Auth\AuthController@getLogout');
 
   // 登録ルート
-  Route::get('auth/register', 'Auth\AuthController@getRegister');
-  Route::post('auth/register', 'Auth\AuthController@postRegister');
+  //Route::get('auth/register', 'Auth\AuthController@getRegister');
+  //Route::post('auth/register', 'Auth\AuthController@postRegister');
 
   Route::get('blog/{slug}', ['as' => 'blog.single', 'uses' => 'BlogController@getSingle']) -> where('slug', '[\w\d\-\_]+');
   Route::get('blog', ['uses' => 'BlogController@getIndex', 'as' => 'blog.index']);
