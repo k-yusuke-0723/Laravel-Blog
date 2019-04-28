2c78f2e218145d00ddf15fe0149bab1130c0afea
Yusuke Kamiyashiki <k.yusuke.0723@gmail.com>
2019-04-29 08:40:10 +0900

    POSTモデルの修正

--- app/Post.php
+++ app/Post.php
@@ -10,5 +10,10 @@ class Post extends Model
     {
       return $this -> belongsTo('App\Category');
     }
+
+    public function tags()
+    {
+      return $this -> belongsToMany('App\Tag');
+    }
 }
 
