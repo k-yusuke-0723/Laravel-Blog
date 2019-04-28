5ffab0dcd26d8baf556fa87416c66a42a3dd23dd
Yusuke Kamiyashiki <k.yusuke.0723@gmail.com>
2019-04-28 18:24:52 +0900

    カテゴリーの表示

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -18,6 +18,10 @@
           <p><a href="{{ route('blog.single', $post -> slug) }}">{{ route('blog.single', $post -> slug) }}</a></p>
         </dl>
         <dl class="dl-horizontal">
+          <label>Category:</label>
+          <p>{{ $post -> category -> name }}</a></p>
+        </dl>
+        <dl class="dl-horizontal">
           <label>Create At:</label>
           <p>{{ date('M j, Y h:ia', strtotime($post -> created_at)) }}</p>
         </dl>
